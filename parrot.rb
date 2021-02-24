b65e20caa77fea31fbd98c725e23411340c64489
CCroteau28 <ccroteau28@gmail.com>
2021-02-23 20:17:48 -0700

    commit tree 7f8d00eb252252b5652a85c411b69f9236745671

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,11 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+# def parrot (bird_sound = "Squawk!")
+#   puts bird_sound
+#   bird_sound
+# end
+
+def parrot(sound="Squawk!")
+    puts sound
+    sound
+end
